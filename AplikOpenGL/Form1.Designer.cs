65da0c79fcb25d1bbc36cb82166eef4a6c6609ec
veranoo <przemas2008@gmail.com>
2013-11-23 00:05:36 +0100

    Wersja czysta

---
+++ AplikOpenGL/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace AplikOpenGL
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.glContr = new SharpGL.OpenGLCtrl();
+			this.SuspendLayout();
+			// 
+			// glContr
+			// 
+			this.glContr.CausesValidation = false;
+			this.glContr.DrawRenderTime = false;
+			this.glContr.FrameRate = 20F;
+			this.glContr.GDIEnabled = false;
+			this.glContr.Location = new System.Drawing.Point(8, 8);
+			this.glContr.Name = "glContr";
+			this.glContr.Size = new System.Drawing.Size(500, 500);
+			this.glContr.TabIndex = 0;
+			this.glContr.OpenGLDraw += new System.Windows.Forms.PaintEventHandler(this.openGLControl1_OpenGLDraw);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(520, 525);
+			this.Controls.Add(this.glContr);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private SharpGL.OpenGLCtrl glContr;
+	}
+}
+
---
+++ AplikOpenGL/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using SharpGL;
+
+namespace AplikOpenGL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.glContr.OpenGLDraw += new System.Windows.Forms.PaintEventHandler(this.openGLControl1_OpenGLDraw);			
+        }
+        double x = 0, y = 5;
+        int[,] s = { {0,5,5,5}, {-1,4,4,4}, {-1,3,1,3}, {1,2,2,2}, {0,1,1,1}, {0,0,5,0}, {0,0,0,3},
+                     {2,1,2,4}, {3,0,3,3}, {4,1,4,4}, {5,0,5,5}
+                   };
+
+
+
+        private void openGLControl1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            //  Get the OpenGL object, just to clean up the code.
+            SharpGL.OpenGL gl = this.glContr.OpenGL;
+
+            gl.Clear(OpenGL.COLOR_BUFFER_BIT | OpenGL.DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();					// Reset The View
+            gl.Translate(-1.5, -2.5, -10);
+
+
+            //Rysowanie ścian
+            gl.Begin(OpenGL.LINES);
+
+            gl.Color(1.0, 1.0, 1.0);
+
+            for (int i = 0; i < s.GetLength(0); i++)
+            {
+                gl.Vertex(s[i, 0], s[i, 1]); // poczatek
+                gl.Vertex(s[i, 2], s[i, 3]); // koniec
+            }
+
+
+            gl.End();
+
+            //Rysowanie postaci
+
+            gl.Begin(OpenGL.QUADS);
+
+            gl.Color(1.0, 1.0, 0.0);
+
+
+
+
+
+            gl.End();
+
+
+
+        }
+    }
+}
